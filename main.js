182c3b6c96138898cfb2c4b1da67b1a990029102
blentell <78812236+blentell@users.noreply.github.com>
2022-01-31 14:20:47 -0500

    Update added files for homework

---
+++ main.js
@@ -0,0 +1,34 @@
+const remove = document.querySelector(".remove");
+const add = document.querySelector(".add");
+const counterNumber = document.querySelector(".number");
+const stars = document.querySelector("#stars");
+// Counter
+let counter = 0;
+
+const updateCounter = (input) => {
+	counter = input;
+	counterNumber.innerText = counter;
+};
+remove.addEventListener("click", () => {
+	updateCounter(counter - 1);
+	if (counter >= 0) {
+		stars.removeChild(stars.lastElementChild);
+	}
+});
+add.addEventListener("click", () => {
+	updateCounter(counter + 1);
+	stars.appendChild(StarComponent());
+});
+
+const StarComponent = () => {
+	if (counter > 0) {
+		const starsImage = document.createElement("div");
+		starsImage.innerHTML = `
+  <img
+  class="starIMG"
+  src="https://previews.123rf.com/images/ayzek/ayzek1107/ayzek110700103/9942926-brushed-gold-star-isolated-on-white-.jpg"
+  alt=""
+  />`;
+		return starsImage;
+	}
+};
